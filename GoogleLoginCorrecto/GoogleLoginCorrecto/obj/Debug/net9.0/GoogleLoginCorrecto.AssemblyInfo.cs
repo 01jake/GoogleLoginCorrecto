d447853ce4a61b806db01367f57c7899dc7c9d15
01jake <ang314256@gmail.com>
2025-06-12 20:33:05 -0700

    first commit

--- GoogleLoginCorrecto/GoogleLoginCorrecto/obj/Debug/net9.0/GoogleLoginCorrecto.AssemblyInfo.cs
+++ GoogleLoginCorrecto/GoogleLoginCorrecto/obj/Debug/net9.0/GoogleLoginCorrecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoogleLoginCorrecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09817ea0bf94cfc18c8591246d25edff375ac694")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoogleLoginCorrecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoogleLoginCorrecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
